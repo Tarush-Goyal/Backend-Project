daa017c03ba958f843c2118e670d6f7229a27cf8
Tarush Goyal <tarush@Tarushs-MacBook-Air.local>
2020-06-10 17:05:29 +0530

    Added insertion of data in MongoDB through Node.js

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
-"use strict";
-var http = require('http');
-var express = require('express');
-var mongo = require('mongodb').MongoClient;
-var assert = require('assert');
-var bodyparser = require('body-parser');
-var app = express();
-var url = 'mongodb://localhost:27017/test';
-var port = process.env.PORT || 3000;
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const mongo = require("mongodb").MongoClient;
+const assert = require("assert");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const url = "mongodb://localhost:27017/test";
+const port = process.env.PORT || 3000;
+
+mongoose.connect(url);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function callback() {
+  console.log("Connected To MongoDB Database");
+});
+const Schema = mongoose.Schema;
+const movieSchema = new Schema({
+  name: String,
+  image: String,
+  summary: String
+});
+
+const moviesModel = mongoose.model("Movie", movieSchema);
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
-var movies = [];
-app.get("/get-movies", function (req, res) {
-    return res.send(movies);
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.get("/get-movies", (req, res) => {
+  moviesModel.find({},(error, movies)=> {
+    res.send(movies);
+  });
 });
-app.use('/', function (req, res, next) {
-    console.log('Connected to Node.js server.');
-    res.setHeader('Content-Type', 'text/html');
-    res.send('<h1>Hello from Node.js!</h1>');
-    next();
+
+app.post("/insert-movies", (req, res) => {
+  console.log(req.body);
+  const movie = new moviesModel(req.body);
+  movie.save(function(err, Movie) {
+    if (err) return console.error(err);
+    console.log(Movie.name + " saved to database.");
+  });
+
+  console.log("insertion complete");
+  res.redirect("/");
 });
-mongo.connect(url, function (err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to MongoDB server");
-    var db = client.db('test');
-    var cursor = db.collection('movies').find();
-    cursor.forEach(function (doc, err) {
-        assert.equal(null, err);
-        movies.push(doc);
-    });
-    client.close();
+
+app.get("/insert-movies", (req, res) => {
+  res.sendFile(path.join(__dirname, "./insert-movies.html"));
 });
-app.listen(port, function () {
-    console.log("running at port " + port);
+
+app.use("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "./home.html"));
+});
+
+app.listen(port, () => {
+  console.log(`running at port ${port}`);
 });
